092d725da8a4ec83af7aca81f6edb220e5952e21
Laharimarada <laharimarada02@gmail.com>
2024-08-28 21:44:01 +0530

    Your commit message

--- src/components/Global.js
+++ src/components/Global.js
@@ -1,14 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import { MultiLevelSidebar } from "./Sidebar";
 import { Outlet } from "react-router-dom";
+
 const Global = () => {
+  const [isSidebarOpen, setSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setSidebarOpen(!isSidebarOpen);
+  };
+
   return (
-    <div className="flex justify-between">
-      <div className="fixed top-0 ">
+    <div className="flex">
+      {/* Toggle Button for Mobile Screens */}
+      <button
+        className="sm:hidden fixed top-4 left-4 z-50 p-2 bg-gray-800 text-white rounded"
+        onClick={toggleSidebar}
+      >
+        ☰
+      </button>
+
+      {/* Sidebar */}
+      <div
+        className={`fixed top-0 ${
+          isSidebarOpen ? "left-0" : "-left-72"
+        } sm:left-0 transition-all duration-300 ease-in-out z-40`}
+      >
         <MultiLevelSidebar />
       </div>
 
-      <div className="ml-72 p-10 bg-gray-300 min-h-screen w-full">
+      {/* Main Content */}
+      <div
+        className={`flex-1 ml-0 sm:ml-72 p-10 bg-gray-300 min-h-screen transition-all duration-300 ${
+          isSidebarOpen ? "ml-72" : ""
+        }`}
+      >
         <Outlet />
       </div>
     </div>
